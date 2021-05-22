0dec94f8371dd44f727765fded4679cfec859829
Eldest Arif Pasirula <eldest.pasirula@hotmail.com>
2021-05-22 17:41:35 +0700

    Add project files.

---
+++ DesignPatternLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
